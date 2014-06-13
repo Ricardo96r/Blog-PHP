4e6649436883bee9c2f37d4993c49b6a3f9ac965
Ricardo Rodríguez <ricardorodriguezgonzalez0@gmail.com>
2014-06-13 13:36:27 -0430

    Aside

--- temas/original/aside.php
+++ temas/original/aside.php
@@ -5,7 +5,7 @@
 if ($page != 'perfil') {?>
 	<div class="affix-aside" data-spy="affix" data-offset-top="60" style="width:320px;">
 <?php } else {?>
-	<div class="affix-aside-perfil" data-spy="affix" data-offset-top="200" style="width:320px;">
+	<div class="affix-aside-perfil" data-spy="affix" data-offset-top="240" style="width:320px;">
 	<?php }?>
 	<div class="col-xs-12 col-sm-6 col-md-12 well-bl-1">
     <?php publicidad(); ?>
