9bcd6f4927c71c7d8c2d41b12edcdf7b9448a9cb
Ricardo Rodríguez <ricardorodriguezgonzalez0@gmail.com>
2014-07-13 20:35:37 -0430

    Buscador

--- temas/original/buscar.php
+++ temas/original/buscar.php
@@ -17,15 +17,40 @@
 		}?>
 </div>
 <?php } else {
-	$buscar_o = $db->query("
+	$q = antiSqlInjection($_GET['q']);?>
+	<div class="well-bl-form">
+        <h1>Buscar: <small><?php echo $q;?></small></h1>
+	</div><?php
+	$bsql = "
 	SELECT cuentas.idcuenta, cuentas.cuenta, cuentas.nombre, cuentas.imagen_perfil, cuentas.imagen_perfil_fondo, publicaciones.idpublicacion, publicaciones.publicacion, publicaciones.tiempo_de_creacion, publicaciones.imagenes_idimagenes, publicaciones.puntos, imagenes.idimagenes, imagenes.ruta
 	FROM cuentas
 	INNER JOIN publicaciones 
 	ON cuentas.idcuenta = publicaciones.cuentas_idcuenta
 	INNER JOIN imagenes
 	ON publicaciones.imagenes_idimagenes = imagenes.idimagenes
-	WHERE publicacion  LIKE '%".$_GET['q']."%'");
+	WHERE publicacion  LIKE '%".$q."%'";
+	
+	$bcount_o = $db->query($bsql);
+	$bcount = ($bcount_o->num_rows);
+
+	if (isset($_GET['bp']) and is_numeric($_GET['bp']) and $_GET['bp'] >= 0) {
+		if (($_GET['bp'] + 0.1) <= (($bcount / 10))) {
+			$getbp=$_GET['bp'];
+		} else {
+			header('Location: ?&p=404');  
+		}
+	} else {
+		$getbp=0;
+		}
+	
+	$getbp_2 = $getbp * 10;
+	$buscar_o = $db->query($bsql."LIMIT ".$getbp_2.",5");
+	
 	while ($buscar = $buscar_o->fetch_array()) {
 		post($buscar);
 		}
+
+	$link = '?p=buscar&q='.$q.'&bp';
+	
+	mostrar_mas($getbp, $bcount, $link);
 	}
